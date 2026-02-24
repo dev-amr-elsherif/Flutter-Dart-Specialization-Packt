1a503039873c6e6da5a594e5cab45081fa3a4e18
Amr Fathy Elsherif <dev.amrelsherif@gmail.com>
2026-02-24 06:38:22 +0200

    docs: implement variables, strings, and integers examples

---
+++ Course_01_Getting_Started/fortune_cookie/lib/dart_basics/variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello Dart!");
+
+  var name = "Amr";
+  int age = 21;
+  print(name);
+  print(age);
+  bool isTall = true;
+  print("$name is $age years old.");
+}
+
+
+// () => Parenthesis
+// {} => Curly brackets
