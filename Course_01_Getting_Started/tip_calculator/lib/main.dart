647137799892e14935fee1282cf7898d6d7463b8
Amr Fathy Elsherif <dev.amrelsherif@gmail.com>
2026-03-12 10:13:09 +0200

    feat: add initial implementation of UTip app with basic UI

--- Course_01_Getting_Started/tip_calculator/lib/main.dart
+++ Course_01_Getting_Started/tip_calculator/lib/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'UTip',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const UTip(),
+    );
+  }
+}
+
+class UTip extends StatefulWidget {
+  const UTip({super.key});
+
+  @override
+  State<UTip> createState() => _UTipState();
+}
+
+class _UTipState extends State<UTip> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("UTip")),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: const Column(
+              children: [Text("Total per person"), Text("\$23.05")],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
