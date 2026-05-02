a268c909bf030e9a8742c119e2d52f835d56c1b1
Amr Fathy Elsherif <dev.amrelsherif@gmail.com>
2026-05-02 23:08:26 +0300

    feat: refactor tip percentage slider into a separate TipSlider widget

--- Course_01_Getting_Started/tip_calculator/lib/main.dart
+++ Course_01_Getting_Started/tip_calculator/lib/main.dart
@@ -165,17 +165,13 @@ class _UTipState extends State<UTip> {
                     ),
 
                     // --- Tip Percentage Slider ---
-                    Slider(
-                      value: _tipPercentage,
-                      onChanged: (value) {
+                    TipSlider(
+                      tipPercentage: _tipPercentage,
+                      onChanged: (double value) {
                         setState(() {
                           _tipPercentage = value;
                         });
                       },
-                      min: 0.0,
-                      max: 0.5,
-                      divisions: 5,
-                      label: '${(_tipPercentage * 100).round()}',
                     ),
                   ],
                 ),
@@ -184,6 +180,34 @@ class _UTipState extends State<UTip> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class TipSlider extends StatelessWidget {
+  const TipSlider({
+    super.key,
+    required double tipPercentage,
+    required this.onChanged,
+  }) : _tipPercentage = tipPercentage;
+
+  final double _tipPercentage;
+  final ValueChanged<double> onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slider(
+      value: _tipPercentage,
+      onChanged: onChanged,
+      // onChanged: (value) {
+      // setState(() {
+      //   _tipPercentage = value;
+      // });
+      // },
+      min: 0.0,
+      max: 0.5,
+      divisions: 5,
+      label: '${(_tipPercentage * 100).round()}',
     );
   }
 }
