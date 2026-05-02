ac21986c9544c08a3c124621b7777df34fb95b09
Amr Fathy Elsherif <dev.amrelsherif@gmail.com>
2026-05-02 23:12:17 +0300

    feat: move TipSlider widget to its own file for better organization

--- Course_01_Getting_Started/tip_calculator/lib/main.dart
+++ Course_01_Getting_Started/tip_calculator/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tip_calculator/widgets/person_counter.dart';
+import 'package:tip_calculator/widgets/tip_slider.dart';
 
 // === Application Entry Point ===
 void main() {
@@ -180,34 +181,6 @@ class _UTipState extends State<UTip> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class TipSlider extends StatelessWidget {
-  const TipSlider({
-    super.key,
-    required double tipPercentage,
-    required this.onChanged,
-  }) : _tipPercentage = tipPercentage;
-
-  final double _tipPercentage;
-  final ValueChanged<double> onChanged;
-
-  @override
-  Widget build(BuildContext context) {
-    return Slider(
-      value: _tipPercentage,
-      onChanged: onChanged,
-      // onChanged: (value) {
-      // setState(() {
-      //   _tipPercentage = value;
-      // });
-      // },
-      min: 0.0,
-      max: 0.5,
-      divisions: 5,
-      label: '${(_tipPercentage * 100).round()}',
     );
   }
 }
